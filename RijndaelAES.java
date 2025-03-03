75be7d0e0ca34b7d3f937c3c4d7214a518892113
chandini_1214 <chandinidaneti@gmail.com>
2025-03-03 14:41:42 +0530

    RijndaelAES.java

--- RijndaelAES.java
+++ RijndaelAES.java
@@ -1,8 +1,9 @@
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 import java.util.Scanner;
+
 public class RijndaelAES {
     public static String encrypt(String plainText, SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
@@ -10,21 +11,25 @@ public class RijndaelAES {
         byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
+
     public static String decrypt(String encryptedText, SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
         return new String(decryptedBytes);
     }
+
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(128); 
-            SecretKey secretKey = keyGenerator.generateKey();
+            String keyString = "1234567890123456";
+            SecretKey secretKey = new SecretKeySpec(keyString.getBytes(), "AES");
+            
             System.out.print("Enter text to encrypt: ");
             String plainText = scanner.nextLine();
+            
             String encryptedText = encrypt(plainText, secretKey);
             System.out.println("Encrypted Text: " + encryptedText);
+            
             String decryptedText = decrypt(encryptedText, secretKey);
             System.out.println("Decrypted Text: " + decryptedText);
         } catch (Exception e) {
